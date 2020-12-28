f8ff9caddd9d00beec0dbd8f03e3d6da2e2baa40
Hkc-1673 <habibkc71@gmail.com>
2020-12-28 21:46:42 +0200

    modified project and completed

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,21 +1,10 @@
-"""todo_proj URL Configuration
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import home, todo_list, todo_create, todo_update, todo_delete
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("todo.urls"))
+    path("", home, name="house"),
+    path("list/", todo_list, name="todo-list"),
+    path("create/", todo_create, name="todo-create"),
+    path("<int:id>/update/", todo_update, name="todo-update"),
+    path("<int:id>/delete/", todo_delete, name="todo-delete")
 ]
